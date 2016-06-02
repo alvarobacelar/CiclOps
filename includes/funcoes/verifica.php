13729cec563e95eec7f8d96abd24d46a9cbed25b
Álvaro Bacelar <alvarobacelar@outlook.com>
2016-06-02 15:17:39 -0300

    Primeiro commit do codigo

---
+++ includes/funcoes/verifica.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * Função que força a conexão HTTPS
+ *
+ * function ForceHTTPS() {
+ * if ($_SERVER['HTTPS'] != "on") {
+ *
+ * $url = $_SERVER['SERVER_NAME'];
+ *
+ * $new_url = "https://" . $url . $_SERVER['REQUEST_URI'];
+ * header("Location: $new_url");
+ * exit;
+ * }
+ * }
+ */
+if (! isset ( $_SESSION )) {
+	session_start ();
+}
+if (! isset ( $_SESSION ["idSession"] )) {
+	if (! isset ( $_SESSION ["erro"] )) {
+		$_SESSION ["erro"] = "erro_sessao";
+	}
+	$smarty->assign ( 'logado', 'NAO' );
+	$estaLogado = "NAO";
+	$smarty->assign ( 'nivel', 'NI' );
+	$local = 0;
+} else {
+	// autentica o usuario
+	
+	$nivel = $_SESSION ['nivel'];
+	$funcao = $_SESSION ['funcao'];
+	$smarty->assign ( "nomeUser", $_SESSION ["posto"] . " " . $_SESSION ["nomeGuerra"] );
+	$smarty->assign ( "posto", $_SESSION ["posto"] );
+	$smarty->assign ( "nivel", $nivel );
+	$smarty->assign ( "funcao", $funcao );
+	$smarty->assign ( "versao", "2.0.2" );
+	$estaLogado = "SIM";
+}
+
+// se não existe nenhum usuario logado, manda para a tela de login
+if ($estaLogado == "NAO") {
+	// verifica se houve erro no login
+	if ($_SESSION ["erro"] == "erro") {
+		$smarty->assign ( "erro", "<div class='alert alert-danger' role='alert'>Usuario ou senha não correspondem</div>" );
+	} else {
+		$smarty->assign ( "erro", "&nbsp;" );
+	}
+	unset ( $_SESSION ["erro"] ); // destroi a session do erro
+	                          // chama a tela de login caso não houver session estartada
+	$smarty->assign ( "titulo", " - Login" );
+	$smarty->assign ( "versao", "2.0.2" );
+	$smarty->assign ( "conteudoLogin", "login/login.tpl" );
+	$smarty->display ( "HTMLogin.tpl" );
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once './lib/smarty/config/config.php';
+require_once './includes/funcoes/verifica.php';
+require_once './includes/models/ManipulateData.php';
+
+if ($estaLogado == "SIM") {
+	
+	
+}
